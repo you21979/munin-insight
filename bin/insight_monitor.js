19d87dbf43535836b3cf01d84e55dd2fa16b2a38
you21979 <you2197901@gmail.com>
2016-09-27 22:10:04 +0900

    refactor

--- bin/insight_monitor.js
+++ bin/insight_monitor.js
@@ -7,11 +7,12 @@ const SYNC = "/sync"
 const STATUS = "/status"
 const ESTIMATEFEE = "/utils/estimatefee"
 
-const get = (base_url, method, params) => rp({uri:base_url + method, qs: params, timeout:5000, method: 'GET', transform : (res) => JSON.parse(res)})
+const get = (base_url, method, params) =>
+    rp({uri:base_url + method, qs: params, timeout:5000, method: 'GET', transform: (res) => JSON.parse(res)})
 
 const arg = () => {
     return {
-        BASE_URL : process.env['BASE_URL'] || 'https://insight.bitpay.com/api',
+        BASE_URL: process.env['BASE_URL'] || 'https://insight.bitpay.com/api',
     }
 }
 
@@ -20,29 +21,8 @@ const initialize = () => {
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 }
 
-const finalize = (list, resp_time) => {
-    const a = (sync) => {
-        const g = new munin.Graph('insight sync height','height','insight');
-        g.add(new munin.Model.Default('bitcoin').setValue(sync.height));
-        return g;
-    }
-    const b = (fee) => {
-        const g = new munin.Graph('insight network estimatefee','satoshi','insight');
-        g.add(new munin.Model.Default('fee').setValue(Math.round(fee['2'] * 1e8)));
-        return g;
-    }
-    const c = (status) => {
-        const g = new munin.Graph('insight network difficulty','difficulty','insight');
-        g.setScale(true);
-        g.add(new munin.Model.Default('bitcoin').setValue(status.difficulty));
-        return g;
-    }
-    const d = (resp_time) => {
-        const g = new munin.Graph('insight response','sec','insight');
-        g.add(new munin.Model.Default('response').setValue(resp_time));
-        return g;
-    }
-    return munin.create([a(list[0]), b(list[1]), c(list[2]), d(resp_time)]);
+const finalize = (list) => {
+    return munin.create(list);
 }
 
 const main = (arg) => {
@@ -54,12 +34,31 @@ const main = (arg) => {
     const beginTime = process.uptime()
 
     return task.seq([
-        () => sync(),
-        () => estimatefee({nbBlocks:2}),
-        () => status({q:"getDifficulty"})
+        () => sync().then(sync => {
+            const g = new munin.Graph('insight sync height','height','insight');
+            g.add(new munin.Model.Default('bitcoin').setValue(sync.height));
+            return g;
+        }),
+        () => estimatefee({nbBlocks:2}).then(fee => {
+            const g = new munin.Graph('insight network estimatefee','satoshi','insight');
+            g.add(new munin.Model.Default('fee').setValue(Math.round(fee['2'] * 1e8)));
+            return g;
+        }),
+        () => status({q:"getDifficulty"}).then(status => {
+            const g = new munin.Graph('insight network difficulty','difficulty','insight');
+            g.setScale(true);
+            g.add(new munin.Model.Default('bitcoin').setValue(status.difficulty));
+            return g;
+        })
     ]).then(list => {
-        finalize(list, process.uptime() - beginTime)
+        const t = (resp_time) => {
+            const g = new munin.Graph('insight response','sec','insight');
+            g.add(new munin.Model.Default('response').setValue(resp_time));
+            return g
+        }
+        return finalize(list.concat(t(process.uptime() - beginTime)))
     })
 }
 
 main(arg())
+
